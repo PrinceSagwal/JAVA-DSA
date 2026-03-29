7e005ff46501babbe288c895b55f672d85364c46
PrinceSagwal <princesagwal123@gmail.com>
2026-03-29 13:15:00 +0530

    Add type promotion example

---
+++ CHAPTER_2/TypePromotion.java
@@ -0,0 +1,12 @@
+public class TypePromotion {
+    public static void main(String[] args) {
+        int a=10;
+        float b=10.25f;
+        long c=25;
+        double d=30;
+        double ans=a+b+c+d;
+        System.out.println(ans);
+
+    }
+    
+}
