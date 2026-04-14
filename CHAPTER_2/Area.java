d2e16cf12d8fbb79556c6e5b1cf871aac8a66d15
PrinceSagwal <princesagwal123@gmail.com>
2026-04-14 10:35:56 +0530

    Add circle area calculation using user input in Java

---
+++ CHAPTER_2/Area.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        float Area=3.14f *a*a;
+        System.out.println(Area);
+    }
+    
+}
