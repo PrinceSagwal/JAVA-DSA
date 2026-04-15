5a4c0114f3714fc486e5c1af0a17d26f4916bcc8
PrinceSagwal <princesagwal123@gmail.com>
2026-04-15 22:21:21 +0530

    Add Primitive_Datatype.java demonstrating basic data types in Java

---
+++ CHAPTER_2/Primitive_Datatype.java
@@ -0,0 +1,17 @@
+public class Primitive_Datatype {
+    public static void main(String[] args) {
+        byte a=8;
+        System.out.println(a);
+        char ch='a';
+        System.out.println(ch);
+        boolean var=false;
+      
+        int number=25;
+        //long
+        //double
+        short n=240;
+
+    
+  }
+    
+}
