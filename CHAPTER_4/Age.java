632e6f35822519320e7ee8d8d1f86ee8e9cb17b7
PrinceSagwal <princesagwal123@gmail.com>
2026-04-20 23:37:34 +0530

    Add Java program to check voting eligibility by age

---
+++ CHAPTER_4/Age.java
@@ -0,0 +1,12 @@
+public class Age {
+    public static void main(String[] args) {
+        int age=19;
+        if(age>18){
+            System.out.println("Right to vote");
+        }
+        else{
+            System.out.println("Not eligible");
+        }
+    }
+    
+}
