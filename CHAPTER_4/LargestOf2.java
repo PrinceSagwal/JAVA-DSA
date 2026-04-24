316eaded8b51dce23c4b51a7b83c7761bee367fb
PrinceSagwal <princesagwal123@gmail.com>
2026-04-24 19:28:05 +0530

    Add Java program to find the largest of two numbers

---
+++ CHAPTER_4/LargestOf2.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+
+
+public class LargestOf2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a>b){
+            System.out.println("A is greater");
+        }
+        else{
+            System.out.println("B is greater");
+        }
+    }
+
+    
+}
