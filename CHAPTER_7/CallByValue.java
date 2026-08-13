4a07844171d04143295b1d761d079c4582e09fcf
PrinceSagwal <princesagwal123@gmail.com>
2026-08-13 21:07:38 +0530

    Add call by value swap example

---
+++ CHAPTER_7/CallByValue.java
@@ -0,0 +1,18 @@
+public class CallByValue {
+    //In java we always use call by value 
+    public static void swap(int a,int b){
+        //Swap
+        int temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+    }
+    public static void main(String args[]){
+        //swap - values exchange
+        int a=5;
+        int b=10;
+        swap(a,b);
+    }
+}
