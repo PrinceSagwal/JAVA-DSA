eaa000cff0e4c27c14a68a4cc50430b599966d84
PrinceSagwal <princesagwal123@gmail.com>
2026-08-15 18:32:10 +0530

    Add factorial calculation using loop

---
+++ CHAPTER_7/Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+    public static int factorial(int n) {
+        int f=1;
+
+        for(int i=1;i<=n;i++){
+            f = f*i;
+        }
+        return f;//Factorial
+        
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int fact=factorial(n);
+        System.out.println("Factorial: "+fact);
+    }
+}
