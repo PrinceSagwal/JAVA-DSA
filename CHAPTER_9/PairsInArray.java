16f194504b58fd5417b02fb2805414d6f6f0d19c
PrinceSagwal <princesagwal123@gmail.com>
2026-09-24 21:22:21 +0530

    Add Pairs In Array  in JAVA DSA

---
+++ CHAPTER_9/PairsInArray.java
@@ -0,0 +1,14 @@
+public class PairsInArray {
+    public static void PrintPairs(int num[]) {
+        for(int i=0;i<num.length-1;i++){
+            int curr=num[i];   
+            for(int j=i+1;j<num.length;j++) {
+                System.out.print("("+curr+","+num[j]+") ");
+            }
+            System.out.println();    
+    }}
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,10};
+        PrintPairs(num);
+    }
+}
